600bf06e87200202163979039a96c0e55467e0eb
novak <novaknazar@gmail.com>
2017-05-01 20:09:41 +0300

    Init project commit

---
+++ app/src/main/java/com/company/city_traffic_customer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.company.city_traffic_customer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
