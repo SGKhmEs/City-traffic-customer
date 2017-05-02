7a88eadf8d44e0829a430dc7a6f7ba1ec65da2d8
novak <novaknazar@gmail.com>
2017-05-02 11:27:57 +0300

    develop database

---
+++ app/src/main/java/com/company/city_traffic_customer/App.java
@@ -0,0 +1,12 @@
+package com.company.city_traffic_customer;
+
+import com.activeandroid.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
